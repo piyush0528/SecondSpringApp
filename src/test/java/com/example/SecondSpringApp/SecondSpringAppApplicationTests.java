c672e5d7ddf2e471482bf9d836e62fe67c5a8be6
piyush0528 <tiwaripiyush262@gmail.com>
2025-03-01 23:05:37 +0530

    added: Second Spring app

---
+++ src/test/java/com/example/SecondSpringApp/SecondSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
